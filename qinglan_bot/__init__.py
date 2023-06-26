cb46ed6dc852492540e66f13f7aa62ac8f464c1d
17TheWord <17theword@gmail.com>
2023-06-26 14:45:50 +0800

    fix 插件加载顺序

--- qinglan_bot/__init__.py
+++ qinglan_bot/__init__.py
@@ -1,22 +1,22 @@
 from nonebot import get_driver, require
 from nonebot.plugin import PluginMetadata
 from nonebot.plugin.manager import PluginLoader
-from .config import Config
 
-require("nonebot_plugin_guild_patch")
-
-__plugin_meta__ = PluginMetadata(
-    name="青岚Bot",
-    description="基于NoneBot的与Minecraft Server互通消息的机器人",
-    homepage="https://github.com/17TheWord/qinglan_bot",
-    usage="配置完成后在群聊发送消息即可同步至 Minecraft 服务器",
-    config=Config,
-    type="application",
-    supported_adapters={
-        "nonebot.adapters.onebot.v11"
-    }
-)
 if isinstance(globals()["__loader__"], PluginLoader):
+    require("nonebot_plugin_guild_patch")
+    from .config import Config
+
+    __plugin_meta__ = PluginMetadata(
+        name="青岚Bot",
+        description="基于NoneBot的与Minecraft Server互通消息的机器人",
+        homepage="https://github.com/17TheWord/qinglan_bot",
+        usage="配置完成后在群聊发送消息即可同步至 Minecraft 服务器",
+        config=Config,
+        type="application",
+        supported_adapters={
+            "nonebot.adapters.onebot.v11"
+        }
+    )
     driver = get_driver()
 
     plugin_config: Config = Config.parse_obj(get_driver().config)
@@ -24,11 +24,13 @@ if isinstance(globals()["__loader__"], PluginLoader):
     from .ws_server import start_ws_server, stop_ws_server
     from . import plugins
 
+
     # Bot 连接时
     @driver.on_bot_connect
     async def on_start():
         # 启动 WebSocket 服务器
         await start_ws_server()
+
 
     # Bot 断开时
     @driver.on_bot_disconnect
