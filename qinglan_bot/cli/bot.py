5a62e3d702621e15ee8c8f0d773d486a8df898b8
17TheWord <17theword@gmail.com>
2023-06-26 14:50:24 +0800

    fix require nonebot-plugin-gocqhttp

--- qinglan_bot/cli/bot.py
+++ qinglan_bot/cli/bot.py
@@ -3,8 +3,8 @@ import importlib
 import nonebot
 
 from os import path
-from nonebot.adapters.onebot.v11 import Adapter
 from nonebot.log import default_format, logger
+from nonebot.adapters.onebot.v11 import Adapter
 
 nonebot.init()
 driver = nonebot.get_driver()
@@ -13,13 +13,14 @@ app = nonebot.get_asgi()
 
 # 删除 qinglan_bot 导入，否则 nonebot 导入时会忽略
 del sys.modules["qinglan_bot"]
+# nonebot.require("nonebot_plugin_guild_patch")
 nonebot.load_plugin("qinglan_bot")
 
 tqdm_loader = importlib.util.find_spec("nonebot_plugin_gocqhttp")
 if tqdm_loader:
-    nonebot.load_plugin("nonebot_plugin_gocqhttp")
+    nonebot.require("nonebot_plugin_gocqhttp")
 else:
-    nonebot.logger.info("未找到 nonebot_plugin_gocqhttp 包，请使用go-cqhttp独立连接程序")
+    nonebot.logger.info("未找到 nonebot_plugin_gocqhttp，请使用go-cqhttp独立连接程序")
 
 # Modify some config / config depends on loaded configs
 #
