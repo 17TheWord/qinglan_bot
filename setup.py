ae52a02f9103afbcaed1c0e43a148280bf4c5ce8
17TheWord <17theword@gmail.com>
2023-07-03 11:29:15 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -4,25 +4,26 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="qinglan-bot",  # 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
-    version="0.1.4",  # 程序版本
-    author="17TheWord",  # 项目作者
-    author_email="17theword@gmail.com",  # 作者邮件
-    description="基于NoneBot的QQ群聊与Minecraft Server消息互通机器人",  # 项目的一句话描述
-    long_description=long_description,  # 加长版描述？
-    long_description_content_type="text/markdown",  # 描述使用Markdown
-    url="https://github.com/17TheWord/qinglan_bot",  # 项目地址
+    name="qinglan-bot",
+    version="0.1.5",
+    author="17TheWord",
+    author_email="17theword@gmail.com",
+    description="基于NoneBot的QQ群聊与Minecraft Server消息互通机器人",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/17TheWord/qinglan_bot",
     packages=find_packages(include=['qinglan_bot', 'qinglan_bot.*']),
     classifiers=[
-        "Programming Language :: Python :: 3.9",  # 使用Python3.9
-        "License :: OSI Approved :: GNU Affero General Public License v3",  # 开源协议
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'mcqq-tool==0.0.4',
+        'mcqq-tool>=0.0.8',
         'nonebot2>=2.0.0',
         'nonebot-adapter-onebot>=2.1.5',
         'nonebot-plugin-guild-patch>=0.2.0',
+        'nonebot2[websockets]',
         'websockets>=10.3',
         'tortoise-orm>=0.19.3',
         'aio-mc-rcon>=3.2.0',
